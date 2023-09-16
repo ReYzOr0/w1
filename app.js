c619346b6ae285b39362b7b1150610e9689db60f
rustem <15@.ru>
2023-09-16 18:00:23 +0300

    a

---
+++ app.js
@@ -0,0 +1,21 @@
+var win = prompt("Введите свой возраст")
+
+ if(win == 1 || win == 2 || win == 3 || win == 4 || win == 5 || win == 6 || win == 7 || win == 8 || win == 9 || win == 10 || win == 11 || win == 12 || win == 13 || win == 14 || win == 15 || win == 16 || win == 17 || win == 18 ) {
+    console.log(" Вам нужно учиться ");
+    alert ("Вам нужно учиться")
+}else if(win == 19 || win == 20 || win == 21 || win == 22 || win == 23 || win == 24 || win == 25 || win == 26 || win == 27 || win == 28 || win == 29 || win == 30 || win == 31 || win == 32 || win == 33 || win == 34 || win == 35 || win == 36 || win == 37 || win == 38 || win == 39 || win == 40 || win == 41 || win == 42 || win == 43 || win == 44 || win == 45 || win == 46 || win == 47 || win == 48 || win == 49 || win == 50) {
+    console.log(" Вам нужно работать ");
+    alert("Вам нужно работать")
+}else if(win == 51 || win == 52 || win == 53 || win == 54 || win == 55 || win == 56 || win == 57 || win == 58 || win == 59 ){
+    console.log(" Вам скоро на пенсию ");
+    alert("Вам скоро на пенсию")
+}else if(win == 60 || win == 61 || win == 62 || win == 63 || win == 64 || win == 65 || win == 66 || win == 67 || win == 68 || win == 69 || win == 70 || win == 71 || win == 72 || win == 73 || win == 74 || win == 75 || win == 76 || win == 77 || win == 78 || win == 79 || win == 80 || win == 81 || win == 82 || win == 83 || win == 84 || win == 85 || win == 86 || win == 87 || win == 88 || win == 89 || win == 90 || win == 91 || win == 92 || win == 93 || win == 94 || win == 95 || win == 96 || win == 97 || win == 98 || win == 99 || win == 100) {
+    console.log(" Вы пенсионер ");
+    alert("Вы пенсионер")
+}
+
+else  {
+    console.log("Что-то пошло не так");
+    alert("Что-то пошло не так")
+}
+      
